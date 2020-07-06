c1273b1e1fb942fef4fdbf39d00c558f430bd1f5
devdave0 <dbchung24@gmail.com>
2020-07-06 15:31:10 -0400

    added crud to hike

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,44 @@
 class HikesController < ApplicationController
+    before_action :find_hike, only: [:show, :edit, :update, :destroy]
+
+    def index 
+        @hikes = Hike.all
+    end 
+
+    def show 
+    end 
+
+    def new 
+        @hike = Hike.new
+    end 
+
+    def create 
+        @hike = Hike.create(hike_params)
+        redirect_to hike_path(@hike)
+    end 
+
+    def edit 
+    end 
+
+    def update 
+        @hike.update(hike_params)
+        redirect_to hike_path(@hike)
+    end 
+
+    def destroy 
+        @hike.destroy 
+        redirect_to hikes_path
+    end 
+
+    private 
+
+    def find_hike 
+        @hike = Hike.find(params[:id])
+    end 
+
+    def hike_params 
+        params.require(:hike).permit(:name, :user_id, :trail_id)
+    end 
+
+
 end
--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,4 +1,8 @@
 class Hike < ApplicationRecord
     belongs_to :user 
     belongs_to :trail 
+
+    def trail=(trail_name)
+        self.trail = Trail.find_or_create_by(trail: trail_name) 
+    end 
 end
